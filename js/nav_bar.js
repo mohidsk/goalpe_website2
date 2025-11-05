1a993e14238c307097ae3eca78731018e50de0e7
mohidsk <ssk138848@gmail.com>
2025-11-05 13:51:03 +0530

    Initial commit

---
+++ js/nav_bar.js
@@ -0,0 +1,9 @@
+function handleOpenSideBar() {
+ document.getElementById("navigation-side-bar")
+ .style.display = "flex";
+}
+
+function handleCloseSideBar() {
+    document.getElementById("navigation-side-bar")
+    .style.display = "none";
+}
